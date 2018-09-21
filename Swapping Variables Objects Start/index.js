3656b46aa03e6629851c0f1ce08fd2a33b5832dd
Taylor Baines <t.baines@share.epsb.ca>
2018-09-21 13:53:09 -0600

    new

---
+++ Swapping Variables Objects Start/index.js
@@ -0,0 +1,20 @@
+console.log('Hello World');
+
+// Create 10 Variables about myself
+let TaylorBaines = {
+ firstName: "Taylor",
+ lastName: "Baines",
+ age: 16,
+ ageAfraid: null,
+ hair: null,
+ male: true,
+ female: false,
+}
+
+//printing out entire project
+console.log(TaylorBaines)
+
+console.log(TaylorBaines.firstName);
+
+
+//console.log("Am I male? " + male);
