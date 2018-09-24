7e230971eda88ddf5915d372309141b2d027121e
Taylor Baines <t.baines@share.epsb.ca>
2018-09-24 13:49:03 -0600

    new

---
+++ Function Start 1/index.js
@@ -0,0 +1,14 @@
+//hello world function
+function greet (firstName, lastName){
+  console.log('Hello ' + firstName + ' World ' + lastName);
+}
+function square (number) {
+  return number*number;
+}
+
+// Create 10 Variables about myself
+let TaylorBaines = ["Taylor", "Baines", 16, null, null, true, false];
+let StephaneMunro = ["Stephane", "Munro", 16, null, null, true, false];
+
+//calling functions, with arguments
+greet(TaylorBaines[0], TaylorBaines[1])
---
+++ Swapping Variables Function/index.js
@@ -0,0 +1,22 @@
+//Function goes here
+
+
+let TaylorBaines = [];
+let StephaneMunro = [];
+
+// firstName, lastName, age, ageAfraid, hair, male, female
+TaylorBaines = ["Taylor", "Baines", 16, null, null, true, false];
+StephaneMunro = ["Stephane", "Munro", 16, null, null, true, false];
+
+//code goes here
+TaylorBaines[1] = StephaneMunro[1];
+
+// Print First names only
+console.log("Last name is: " + TaylorBaines[1]);
+console.log("Last name is: " + StephaneMunro[1]);
+
+TaylorBaines [1]="Is Awesome";
+console.log(TaylorBaines);
+
+
+//console.log("Am I male? " + male);
