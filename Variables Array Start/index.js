c4ab6cccec46a9073ba452594224afe44cd0e9e3
Taylor Baines <t.baines@share.epsb.ca>
2018-09-24 13:14:46 -0600

    new

---
+++ Variables Array Start/index.js
@@ -0,0 +1,21 @@
+// Create 10 Variables about myself
+let TaylorBaines = [];
+let StephaneMunro = [];
+
+// firstName, lastName, age, ageAfraid, hair, male, female
+TaylorBaines = ["Taylor", "Baines", 16, null, null, true, false];
+StephaneMunro = ["Stephane", "Munro", 16, null, null, true, false];
+
+//printing out entire
+console.log(TaylorBaines);
+console.log(StephaneMunro);
+
+// Print First names only
+console.log("Last name is: " + TaylorBaines[1]);
+console.log("Last name is: " + StephaneMunro[1]);
+
+TaylorBaines [1]="Is Awesome";
+console.log(TaylorBaines);
+
+
+//console.log("Am I male? " + male);
