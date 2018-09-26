8469519109055ba79a93dcbf57b436d29c4b1736
Taylor Baines <t.baines@share.epsb.ca>
2018-09-26 13:40:44 -0600

    new

---
+++ Operators/index.js
@@ -0,0 +1,20 @@
+function simpleOperators (numberA, numberB){
+
+}
+
+let number = [4, 7];
+
+simpleOperators (numberA, numberB);
+let adding = numberA + numberB;
+let subtract = numberA - numberB;
+let multiply = numberA * numberB;
+let divide = numberA / numberB;
+let skipCount
+
+
+console.log(number);
+
+console.log(adding);
+console.log(subtract);
+console.log(multiply);
+console.log(divide);
