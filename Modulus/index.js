42e76739eff0a899f5c89374fb194b274211480c
Taylor Baines <t.baines@share.epsb.ca>
2018-10-03 14:09:56 -0600

    new

--- Modulus/index.js
+++ Modulus/index.js
@@ -1,6 +1,11 @@
 let number = [1, 20]; //Boundary Numbers
+console.log("First number in sequence is " + number [0]);
+console.log("Last number in sequence is " + number [1]);
+
 let skipCount = 1; //Number of skipCounting between each element of the sequence, used in FOR Loop
 let difference = number[1] - number[0]; //Difference, mandatory expectation
+console.log ("The difference of the two numbers is " + difference);
+
 let startingNumber = number[0]; //Hint: used in the FOR Loop
 for (let i=0; i<=difference; i++){
   even = (startingNumber += skipCount) % 2;
