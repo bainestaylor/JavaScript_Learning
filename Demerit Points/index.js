6c0f1b63d042fe76d72d37578bd515891ea4affc
Taylor Baines <t.baines@share.epsb.ca>
2018-10-26 13:16:52 -0600

    new

--- Demerit Points/index.js
+++ Demerit Points/index.js
@@ -1,13 +1,15 @@
-checkSpeed (68); //add value here
+checkSpeed (222); //add value here
 
 function checkSpeed (speed) {
   speedLimit = 70; //add value here, remember to initialize with constant not let for security purposes
-  kmPerPoint = ; //add value here
-  if (speed <= ) return console.log ('Ok'); //Add formula for calculating the acceptable speed from above variables
+  kmPerPoint = 5; //add value here
+  if (speed <= 74) return console.log ('Ok'); //Add formula for calculating the acceptable speed from above variables
   //after return, none of the other code will run, there is no need to report demerit points
 
   // This variable and formula are const (constant) for security so it cannot be changed, for example when cross-scripting
-  const points = Math.floor (); //Add formula for calculating the point demerits
-  if () return console.log ('License Suspended'); //Add conditional to check for points to suspend license
-  else console.log ('Points', points)
-}
+  const points = Math.floor ();
+
+
+  //Add formula for calculating the point demerits
+  if (points >= 12) return console.log ('License Suspended'); //Add conditional to check for points to suspend license
+  else return console.log ('Points', points);
